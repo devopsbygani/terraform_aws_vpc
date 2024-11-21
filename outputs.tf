98d0a68e461fc1b03ada73e4aa2ad9ea3c712d7c
ganeshmoka <ganeshmoka1999@gmail.com>
2024-11-21 06:45:40 +0530

    dev module

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,15 @@ output "routes_table_info" {
   value = data.aws_route_table.main
 }
 
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
+
